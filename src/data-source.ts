e71d1516acf940c1b467466b3a6434ca369a629d
Pedro Silva <pedrosilvacwb@gmail.com>
2023-01-04 11:59:30 -0300

    deploy configs

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,24 @@
-import "dotenv/config";
-import path from "path";
-import "reflect-metadata";
-import { DataSource, DataSourceOptions } from "typeorm";
+import 'dotenv/config';
+import path from 'path';
+import 'reflect-metadata';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
 const dataSourceConfig = (): DataSourceOptions => {
-  const entitiesPath: string = path.join(__dirname, "./entities/*.{js,ts}");
-  const migrationsPath: string = path.join(__dirname, "./migrations/*.{js,ts}");
+  const entitiesPath: string = path.join(__dirname, './entities/*.{js,ts}');
+  const migrationsPath: string = path.join(__dirname, './migrations/*.{js,ts}');
 
+  const nodeEnv: string = process.env.NODE_ENV;
+
+  if (nodeEnv === 'production') {
+    return {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
   return {
-    type: "postgres",
+    type: 'postgres',
     host: process.env.PGHOST,
     port: parseInt(process.env.PGPORT),
     username: process.env.PGUSER,
